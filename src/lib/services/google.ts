60a0748ec8750427c18c465c7aca3e9c52fd4318
Aren Hovsepyan <ar.hovsepyan2011@gmail.com>
2025-06-23 17:29:50 +0400

    chore: refactor Google authentication to use environment variables instead of keyfile

--- src/lib/services/google.ts
+++ src/lib/services/google.ts
@@ -1,24 +1,12 @@
-import { resolve } from "path";
-import { readFile } from "fs/promises";
 import { google } from "googleapis";
 import { JWT } from "google-auth-library";
 
-async function getGoogleKeyfile() {
-  const credentials = await readFile(
-    resolve(process.cwd(), "keys", "softgang-search-service-creds.json")
-  );
-
-  return JSON.parse(credentials.toString());
-}
-
 export async function getGoogleAuth() {
-  const keyfile = await getGoogleKeyfile();
-
   const auth = new JWT({
-    email: keyfile.client_email,
-    key: keyfile.private_key,
-    clientId: keyfile.client_id,
-    projectId: keyfile.project_id,
+    email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    key: process.env.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    clientId: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    projectId: process.env.GOOGLE_SERVICE_ACCOUNT_CLIENT_ID,
     scopes: [
       "https://www.googleapis.com/auth/calendar",
       "https://www.googleapis.com/auth/calendar.events",
